7e162a9545972f262fda7ed0974b92fbb59abefd
Stedders <luke.stedman@gmail.com>
2013-01-26 17:47:29 +0000

    Commited Interface for Contact Manager.

---
+++ src/bbk/pij/jsted02/interfaces/FutureMeeting.java
@@ -0,0 +1,9 @@
+package bbk.pij.jsted02.interfaces;
+
+/**
+ * A meeting to be held in the future
+ */
+public interface FutureMeeting extends Meeting{
+	// No methods here, this is just a naming interface
+	// (i.e. only necessary for type checking and/or downcasting)
+}
