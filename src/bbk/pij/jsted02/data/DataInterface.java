efc3a9c9a19ec84daab3b70b645f4cc20dd598e6
Luke Stedman <luke.stedman@gmail.com>
2013-01-28 15:37:06 +0000

    Added data package, moved the serialiser to the new package and added a DataInterface class - single interface for all data interaction.

---
+++ src/bbk/pij/jsted02/data/DataInterface.java
@@ -0,0 +1,14 @@
+/**
+ * DataInterface - Interface for providing data sets to the system, ideally 
+ * 		this will be the single point of contact that the System has with
+ * 		the data, reducing the chance of issues if the code was distributed
+ * 		throughout the system. 
+ */
+package bbk.pij.jsted02.data;
+
+/**
+ * @author Luke Stedman (jsted02), MSc CS Yr1 2012/13
+ */
+public class DataInterface {
+
+}
--- src/bbk/pij/jsted02/data/DataSerialiser.java
+++ src/bbk/pij/jsted02/data/DataSerialiser.java
@@ -2,7 +2,7 @@
  * DataSerialiser - Handler for serialisation and loading of data from txt 
  * 		file. Format of data will be in JSON 
  */
-package bbk.pij.jsted02.utils;
+package bbk.pij.jsted02.data;
 
 /**
  * @author Luke Stedman (jsted02), MSc CS Yr1 2012/13
