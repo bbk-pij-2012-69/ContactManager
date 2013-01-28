9f094f9a44feec6623a23f9ad47646df9739618e
Luke Stedman <luke.stedman@gmail.com>
2013-01-28 14:18:14 +0000

    Added DataSerialiser class, will probably be based on JSON (pending acceptance that adding the JSON libraries is ok).

---
+++ src/bbk/pij/jsted02/utils/DataSerialiser.java
@@ -0,0 +1,12 @@
+/**
+ * DataSerialiser - Handler for serialisation and loading of data from txt 
+ * 		file. Format of data will be in JSON 
+ */
+package bbk.pij.jsted02.utils;
+
+/**
+ * @author Luke Stedman (jsted02), MSc CS Yr1 2012/13
+ */
+public class DataSerialiser {
+
+}
